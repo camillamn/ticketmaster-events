22dc3a44397e191219b20cbc6926b8e33c9c9489
camillamn <112381378+camillamn@users.noreply.github.com>
2023-05-24 16:39:48 +0200

    Refactor code (#18)
    * Added class to index.html
    * Refactored the render module
    * Refactored the fetch module
    * Removed fetch module from main

--- _app/js/modules/fetchEvents.js
+++ _app/js/modules/fetchEvents.js
@@ -1,59 +1,66 @@
 import { apikey } from "../env.js";
-import renderEvents from "./renderEvents.js";
 
-const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
-const city = 'bergen';
-const sort = 'date,asc';
-const perPage = 30;
-let currentPage = 1;
-const options = {
-	method: "GET"
-};
 
-const endpoint = `
-	${baseUrl}
-	?apikey=${apikey}
-	&locale=*
-	&city=${city}
-	&classificationName=music
-	&size${perPage}
-	&page=${currentPage}
-	&sort=${sort}
-	`;
+const warningElement = document.querySelector('.warning');
 
 export default async function fetchEvents() {
+	const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
+	const city = 'bergen';
+	const sort = 'date,asc';
+	const perPage = 30;
+	const page = 1;
+	const classification = 'music';
+	const options = {
+		method: "GET"
+	};
+
+	const endpoint = `
+		${baseUrl}
+		?apikey=${apikey}
+		&locale=*
+		&size=${perPage}
+		&page=${page}
+		&city=${city}
+		&classificationName=${classification}
+		&sort=${sort}
+		`;
+
+		let response = '';
+		if (!response) {
+			loading();
+		}
+
+		console.log(endpoint)
+		response =  await fetch(endpoint, options);
+
 	try {
-		const response = await fetch(endpoint, options);
 		const data = await handleResponse(response);
 		return data._embedded.events;
 	} catch (error) {
 		handleError(error);
 	}
-}
 
-function handleResponse(response) {
-	if(response.ok) {
-		return response.json();
-	} else if (response.status === 404) {
-		 throw new Error('Url not existing');
-	} else if (response.status === 401) {
-		 throw new Error('Not authorized user');
-	} else if (response.status >= 500) {
-		 throw new Error('Server not responding');
-	} else {
-		 throw new Error('Something went wrong');
+	async function handleResponse(response) {
+		if(response.ok) {
+			return response.json();
+		} else if (response.status === 404) {
+			 throw new Error('Url not existing');
+		} else if (response.status === 401) {
+			 throw new Error('Not authorized user');
+		} else if (response.status >= 500) {
+			 throw new Error('Server not responding');
+		} else {
+			 throw new Error('Something went wrong');
+		}
 	}
+	
+	function loading() {
+		console.log('wait');
+	}
+
+	function handleError(error) {
+		warningElement.classList.toggle('hidden');
+		warningElement.textContent = error;
+	}
+
 }
-
-// function loading() {
-// 	console.log('wait');
-// }
-
-function handleError(error) {
-	warningElement.classList.toggel('hidden');
-	warningElement.textContent = error;
-}
-
-fetchEvents().then(events => {
-	renderEvents(events);
-});
